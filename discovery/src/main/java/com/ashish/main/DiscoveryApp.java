539e1b6ad8098d08bc4ddde1ebbb06d34488e9c5
Ashish <Ashish Tripathi>
2018-03-29 22:13:28 +0530

    All

---
+++ discovery/src/main/java/com/ashish/main/DiscoveryApp.java
@@ -0,0 +1,17 @@
+package com.ashish.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@EnableEurekaServer
+@SpringBootApplication
+public class DiscoveryApp {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(DiscoveryApp.class, args); 
+	}
+
+}
---
+++ route/src/main/java/org/route/App.java
@@ -0,0 +1,13 @@
+package org.route;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
